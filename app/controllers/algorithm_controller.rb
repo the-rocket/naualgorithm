deeeabe6a99d67d58ca8bef04fa22612b5d939ea
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2017-02-24 15:51:07 -0600

    algorithm web

---
+++ app/controllers/algorithm_controller.rb
@@ -0,0 +1,2 @@
+class AlgorithmController < ApplicationController
+end
---
+++ test/controllers/algorithm_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AlgorithmControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
