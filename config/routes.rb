5a4d59084ddd4c61af6679dce35d79c21dc91c2c
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2017-02-26 20:31:58 -0600

    checking

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root 'algorithm#index'
-   get '/algorithm' => 'algorithm#index'
+   root :to => 'algorithm#index'
+   resources :blogs
+   
 end
--- db/migrate/20170224230541_create_blogs.rb
+++ db/migrate/20170224230541_create_blogs.rb
@@ -1,7 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration[5.0]
   def change
     create_table :blogs do |t|
-
+      t.string :author
+      t.string :text
       t.timestamps
     end
   end
